f297cd75c0ef7be4f179c6686665bc8aef569574
hashekiii <marksamuel.ku@gmail.com>
2023-07-06 17:51:55 +0800

    Finished Services Section

--- script.js
+++ script.js
@@ -21,3 +21,21 @@ function openmenu() {
 function closemenu() {
     sidemenu.style.right = "-200px";
 }
+
+// Linking Google Sheets to Contact Form
+const scriptURL =
+    "https://script.google.com/macros/s/AKfycbxG_J36MEezXJGnxj7Y40amHl5ILCNOo9PDrizyGx3IYhVIosWrc_j6kH5LYM_cVZ7OLQ/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    fetch(scriptURL, { method: "POST", body: new FormData(form) })
+        .then((response) => {
+            msg.innerHTML = "Message was sent successfully";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 2000);
+        })
+        .catch((error) => console.error("Error!", error.message));
+});
