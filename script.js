52fdf9164ed972fa73821a496e22cc2a40297465
hashekiii <marksamuel.ku@gmail.com>
2023-07-07 00:07:47 +0800

    Added Image Gallery, Fixed Nav Bar

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Experiences Functionality
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
@@ -12,7 +13,10 @@ function opentab(tabname) {
     document.getElementById(tabname).classList.add("active-tab");
 }
 
+// Side Menu Functionality
 var sidemenu = document.getElementById("sidemenu");
+var hamburgerIcon = document.querySelector(".fa-bars");
+var closeIcon = document.querySelector(".fa-xmark");
 
 function openmenu() {
     sidemenu.style.right = "0";
@@ -22,6 +26,33 @@ function closemenu() {
     sidemenu.style.right = "-200px";
 }
 
+function handleClickOutside(event) {
+    if (!sidemenu.contains(event.target) && event.target !== hamburgerIcon) {
+        closemenu();
+    }
+}
+hamburgerIcon.addEventListener("click", openmenu);
+closeIcon.addEventListener("click", closemenu);
+document.addEventListener("click", handleClickOutside);
+
+// Image Gallery Functionality
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+
+function closeImg() {
+    fullImgBox.style.display = "none";
+}
+function outsideClickHandler(event) {
+    // Check if the clicked element is not inside the sidemenu
+    if (!sidemenu.contains(event.target)) {
+        closemenu();
+    }
+}
 // Linking Google Sheets to Contact Form
 const scriptURL =
     "https://script.google.com/macros/s/AKfycbxG_J36MEezXJGnxj7Y40amHl5ILCNOo9PDrizyGx3IYhVIosWrc_j6kH5LYM_cVZ7OLQ/exec";
